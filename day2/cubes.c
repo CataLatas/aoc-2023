3ec957b6446058c751187a849318a3bf40d17a6f
CataLatas <richard_bukor@hotmail.com>
2023-12-03 21:49:36 -0300

    Day 2, part 2

--- day2/cubes.c
+++ day2/cubes.c
@@ -16,64 +16,112 @@ char* EXAMPLE_INPUT[] = {
 
 #define EXAMPLE_RESULT 8
 
-int determine_possible(char **input, int max_r, int max_g, int max_b) {
+#define EXAMPLE_RESULT_2 2286
+
+#define MAX_CUBES 16
+
+typedef struct {
+    int r;
+    int g;
+    int b;
+} Cubes;
+
+int read_game_cubes(char **line, Cubes cubes[MAX_CUBES]) {
+    int count = 0;
     int n;
-    int fail = 0;
 
-    while (1) {
-        n = strtol(*input, input, 10);
-        assert(**input == ' ');
-        ++(*input); // Skip space
+    while (**line) {
+        n = strtol(*line, line, 10);
+        assert(**line == ' ');
+        ++(*line); // Skip space
 
-        if (**input == 'r') {
-            fail = (n > max_r);
-            *input += strlen("red");
-        } else if (**input == 'g') {
-            fail = (n > max_g);
-            *input += strlen("green");
-        } else if (**input == 'b') {
-            fail = (n > max_b);
-            *input += strlen("blue");
+        if (**line == 'r') {
+            cubes[count].r = n;
+            *line += strlen("red");
+        } else if (**line == 'g') {
+            cubes[count].g = n;
+            *line += strlen("green");
+        } else if (**line == 'b') {
+            cubes[count].b = n;
+            *line += strlen("blue");
         } else {
             assert(0 && "Unreachable");
         }
 
-        if (fail) {
-            return 0;
-        }
+        if (**line == ',' || **line == ';') {
+            if (**line == ';') ++count;
 
-        if (**input == '\0') {
-            break;
+            ++(*line);
         }
-
-        assert(**input == ',' || **input == ';');
-        ++(*input); // Skip ',' or ';'
     }
 
-    return 1;
+    ++count;
+    return count;
+}
+
+int read_game_id(char **line) {
+    // Read XX from "Game XX: ..."
+    int id;
+
+    *line += strlen("Game");
+    id = strtol(*line, line, 10);
+    ++(*line); // Consume ':'
+
+    return id;
 }
 
 void part1(Result *result, char **lines) {
+    int cube_count;
+    int possible;
     int id;
 
     int sum = 0;
     for (char *line = *lines; *lines; line = *(++lines)) {
+        Cubes cubes[MAX_CUBES] = {0};
+
         if (*line == '\0') break; // Filter out empty lines that appear at the end of the file
 
-        line += strlen("Game");
-        id = strtol(line, &line, 10);
-        ++line; // Consume ':'
+        id = read_game_id(&line);
 
-        if (determine_possible(&line, 12, 13, 14)) {
-            sum += id;
+        cube_count = read_game_cubes(&line, cubes);
+
+        possible = 1;
+        for (int i = 0; i < cube_count && possible; ++i) {
+            possible = (cubes[i].r <= 12 && cubes[i].g <= 13 && cubes[i].b <= 14);
         }
+
+        if (possible) sum += id;
     }
 
     result->integer = sum;
 }
 
-void part2(Result *result, char *input) {
-    result->integer = 0;
+void part2(Result *result, char **lines) {
+    int cube_count;
+
+    int sum = 0;
+    for (char *line = *lines; *lines; line = *(++lines)) {
+        Cubes cubes[MAX_CUBES] = {0};
+        int max_r = 0;
+        int max_g = 0;
+        int max_b = 0;
+
+        if (*line == '\0') break; // Filter out empty lines that appear at the end of the file
+
+        read_game_id(&line); // Consume game ID
+
+        cube_count = read_game_cubes(&line, cubes);
+
+        for (int i = 0; i < cube_count; ++i) {
+            if (cubes[i].r > max_r) max_r = cubes[i].r;
+            if (cubes[i].g > max_g) max_g = cubes[i].g;
+            if (cubes[i].b > max_b) max_b = cubes[i].b;
+        }
+
+        sum += max_r * max_g * max_b;
+    }
+
+    result->integer = sum;
 }
 
 int main() {
@@ -88,9 +136,9 @@ int main() {
     printf("Sum of ID of valid games: %d\n\n", result.integer);
 
     printf("----- PART 2 -----\n");
-    //check_example_int(part2, EXAMPLE_INPUT_2, EXAMPLE_RESULT_2);
-    //part2(&result, input);
-    printf("Part 2 TBD\n");
+    check_example_int(part2, EXAMPLE_INPUT, EXAMPLE_RESULT_2);
+    part2(&result, input_lines);
+    printf("Sum of power of sets: %d\n", result.integer);
 
     free(*input_lines);
     free(input_lines);
